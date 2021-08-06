426bab79e7ba91316367562bb72960845289a7d8
lolsof <lolsofi@rambler.ru>
2021-08-06 15:29:28 +0300

    swiper versia cherez function

--- javaScript/brands_software.js
+++ javaScript/brands_software.js
@@ -1,60 +1,90 @@
-const swiper = new Swiper('.swiper-container', {
+// const swiper = new Swiper('.swiper-container', {
+//   slidesPerView: 1.2,
+//   spaceBetween: 16,
+//   pagination: {
+//     el: '.swiper-pagination',
+//     clickable: true,
+//   }
+// });
+
+
+const swiper = {
   slidesPerView: 1.2,
   spaceBetween: 16,
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
-  },
-  // on: {
-  //   init: function () {
-  //     console.log('swiper initialized');
-  //   },
-  // },
-});
+  }
+};
+
+// function Swiper1 (swiper) {
+//   mySwiper = swiper;
+// }
+
 
 const swiperContainer = document.querySelector('.swiper-container');
-const swiperWrapper = document.querySelector('.swiper-wrapper');
+// const swiperWrapper = document.querySelector('.swiper-wrapper');
 const brandList = document.querySelector('.brands__list');
 const swiperPagination = document.querySelector('.swiper-pagination');
 let mySwiper;
 
+let mobile = window.matchMedia('(min-width: 0px) and (max-width: 720px)');
+let tablet = window.matchMedia('(min-width: 768px)');
+let desktop = window.matchMedia('(min-width: 1024px)');
 
 
-let createSwiper = function () {
-  if (!swiperWrapper.classList.contains('swiper-wrapper')) {
-    swiperWrapper.classList.add('swiper-wrapper');
-  }
-  if (swiperWrapper.classList.contains('brands__list')) {
-    swiperWrapper.classList.remove('brands__list');
-  }
-  // if (typeof (mySwiper) == 'undefined') {
-  //   mySwiper = new Swiper('.swiper-container', swiperOptions);
-  //   console.log(`initialized + ${mySwiper}`);
-  // }
-}
+window.addEventListener('resize', function () {
+  resize();
+});
 
-let destroySwiper = function () {
-  if (mySwiper) {
+function resize() {
+  if (tablet.matches) {
+    mySwiper = new Swiper1('.swiper-container', swiper);
+  } else{
     mySwiper.destroy();
-    mySwiper = undefined;
-  }
-  if (swiperWrapper.classList.contains('swiper-wrapper')) {
-    swiperWrapper.classList.remove('swiper-wrapper');
-  }
-  if (!swiperWrapper.classList.contains('brands__list')) {
-    swiperWrapper.classList.add('brands__list');
-  }
-  if (!swiperPagination.classList.contains('swiper-pagination')) {
-    swiperPagination.classList.remove('swiper-pagination');
+
   }
 }
 
-let maxSwiperWindow = 768;
-if (document.body.clientWidth < maxSwiperWindow) {
-  createSwiper();
-} else {
-  destroySwiper();
-}
+
+
+
+
+// let createSwiper = function () {
+//   if (!swiperWrapper.classList.contains('swiper-wrapper')) {
+//     swiperWrapper.classList.add('swiper-wrapper');
+//   }
+//   if (swiperWrapper.classList.contains('brands__list')) {
+//     swiperWrapper.classList.remove('brands__list');
+//   }
+//   // if (typeof (mySwiper) == 'undefined') {
+//   //   mySwiper = new Swiper('.swiper-container', swiperOptions);
+//   //   console.log(`initialized + ${mySwiper}`);
+//   // }
+// }
+
+// let destroySwiper = function () {
+//   if (mySwiper) {
+//     mySwiper.destroy();
+//     mySwiper = undefined;
+//   }
+//   if (swiperWrapper.classList.contains('swiper-wrapper')) {
+//     swiperWrapper.classList.remove('swiper-wrapper');
+//   }
+//   if (!swiperWrapper.classList.contains('brands__list')) {
+//     swiperWrapper.classList.add('brands__list');
+//   }
+//   if (!swiperPagination.classList.contains('swiper-pagination')) {
+//     swiperPagination.classList.remove('swiper-pagination');
+//   }
+// }
+
+// let maxSwiperWindow = 768;
+// if (document.body.clientWidth < maxSwiperWindow) {
+//   createSwiper();
+// } else {
+//   destroySwiper();
+// }
 
 
 
